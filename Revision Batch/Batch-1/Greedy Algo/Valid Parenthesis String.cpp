fc5a2690d9bcbc38fc76e59f8ba5d3d1d67f7586
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-05 19:21:18 +0530

    Update Valid Parenthesis String.cpp

--- Revision Batch/Batch-1/Greedy Algo/Valid Parenthesis String.cpp
+++ Revision Batch/Batch-1/Greedy Algo/Valid Parenthesis String.cpp
@@ -25,6 +25,29 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin=0, leftMax=0;
+        
+        for(char c: s){
+            if(c=='('){
+                leftMin++;  leftMax++;
+            }
+            else if(c==')'){
+                leftMin--;  leftMax--;
+            }
+            else{
+                leftMin--;  //Assuming * is '('
+                leftMax++;  //Assuming * is ')'
+            }
+            if(leftMin<0)   leftMin=0;
+            if(leftMax<0)   return false;
+        }
+        return leftMin==0;
+    }
+};
+
 /*
 - **`leftMin`**: This variable represents the minimum number of open parentheses `(` that could be present at a given point. It assumes the wildcard `` could be treated as a closing parenthesis `)`.
 - **`leftMax`**: This variable represents the maximum number of open parentheses `(` that could be present at a given point. It assumes the wildcard `` could be treated as an opening parenthesis `(`.
