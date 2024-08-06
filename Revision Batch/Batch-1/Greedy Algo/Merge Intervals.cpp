6e9fbc5b13d7efb7e64f65f681057900a21aaa80
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-06 12:36:47 +0530

    Create Merge Intervals.cpp

---
+++ Revision Batch/Batch-1/Greedy Algo/Merge Intervals.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        int start=intervals[0][0], end=intervals[0][1];
+        for(int i=1; i<n; i++){
+            if(intervals[i][0] <= end)  end=max(intervals[i][1],end);
+            else{
+                ans.push_back({start,end});
+                start=intervals[i][0];
+                end=max(intervals[i][1],end);
+            }
+        }
+        ans.push_back({start,end});
+        return ans;
+    }
+};
