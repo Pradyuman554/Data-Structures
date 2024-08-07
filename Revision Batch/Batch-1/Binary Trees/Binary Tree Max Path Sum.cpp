efd4c2da082196f9640503ac6760486a354adb16
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-07 08:25:04 +0530

    Update Binary Tree Max Path Sum.cpp

--- Revision Batch/Batch-1/Binary Trees/Binary Tree Max Path Sum.cpp
+++ Revision Batch/Batch-1/Binary Trees/Binary Tree Max Path Sum.cpp
@@ -8,7 +8,30 @@ public:
 
         ans = max({ans, root->val, l+root->val, r+root->val, l+r+root->val});
 
-        return max(max(l,r)+root->val, root->val);  //Would it even be a good idea to consider the left and right subtrees for our final sum
+        return max(max(l,r)+root->val, root->val);  //Would it even be a good idea to consider the left and right subtrees for our final sum, YA FIR ROOT HI BADHIYA THA
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int ans=INT_MIN;
+        solve(root, ans);
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int solve(TreeNode* root, int& ans){
+        if(!root)   return 0;
+
+        int l = max(0, solve(root->left, ans));
+        int r = max(0, solve(root->right, ans));
+
+        ans = max(ans, l+r+root->val);
+
+        return root->val + max(l,r);
     }
 
     int maxPathSum(TreeNode* root) {
